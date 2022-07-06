563f1125548d4aad2bd6568b9461cc907d6cdf66
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-07-05 21:42:10 -0500

    agrega un navbar funcional

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,18 +3,22 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from '../auth/pages/LoginPage'
 import DcPage from '../heroes/pages/DcPage'
 import MarvelPage from '../heroes/pages/MarvelPage'
+import { Navbar } from '../ui/components'
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="marvel" element={<MarvelPage />} />
-      <Route path="dc" element={<DcPage />} />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
 
-      <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
 
-      <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/" element={<Navigate to="/marvel" />} />
 
-    </Routes>
+      </Routes>
+    </>
   )
 }
 
---
+++ src/ui/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+	const classStyle = (isActive) => isActive ? 'nav-item nav-link active' :  'nav-item nav-link'
+
+	return (
+		<nav className="navbar navbar-expand-sm navbar-dark bg-dark px-4">
+			<Link className="navbar-brand" to="/" >
+				Asociaciones
+			</Link>
+
+				<div className="navbar-collapse">
+					<div className="navbar-nav">
+						<NavLink className={ ({isActive}) => classStyle(isActive) } to="/marvel">
+							Marvel
+						</NavLink>
+
+						<NavLink className={ ({isActive}) => classStyle(isActive) } to="/dc">
+							DC
+						</NavLink>
+					</div>
+				</div>
+
+				<div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+					<ul className="navbar-nav ml-auto">
+						<span className="nav-item nav-link text-primary">
+							Juan
+						</span>
+						<button className="nav-item nav-link btn">
+								Logout
+						</button>
+					</ul>
+				</div>
+		</nav>
+  )
+}
+
+export default Navbar
