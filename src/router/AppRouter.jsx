0771e1ace1c5a7cafe6c31f7fe0bb31d00fa7518
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-07-19 00:25:37 -0500

    crea las rutas publicas y privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,26 @@ import { Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth/pages'
 import { HeroresRoutes } from '../heroes/routes'
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroresRoutes />} />
+        <Route path="login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+      } />
+
+        {/* Rutas privadas */}
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroresRoutes />
+          </PrivateRoute>
+        } />
+
       </Routes>
     </>
   )
---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { AuthContext } from "../auth/context"
+
+const PublicRoute = ({ children }) => {
+  const { authState } = useContext( AuthContext )
+  const { logged } = authState
+
+  return (!logged)
+  ? children
+  : <Navigate to='/marvel' />
+}
+
+export default PublicRoute
