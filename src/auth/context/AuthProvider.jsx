5bb66782551fb207d76023311c00d0b001a43ecf
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-07-18 21:10:22 -0500

    mantener el usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -5,23 +5,36 @@ import AuthReducer from './AuthReducer'
 import types from '../types/types'
 
 
+//el estado inicial del reducer
 const initialState = {
   logged: false
 }
 
+//obtiene el usuario del localStorage
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') )
+
+  return {
+    logged: !!user,
+    user: user
+  }
+}
+
 const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer(AuthReducer, initialState)
+  const [ authState, dispatch ] = useReducer(AuthReducer, initialState, init)
 
+  //cuando se ejecuta la funcion se guarda un nuevo usuario
   const login = ( name = '' ) => {
+
+    const user = { id: 'ABC', name: name }
 
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name
-      }
+      payload: user
     }
+
+    localStorage.setItem( 'user', JSON.stringify( user ) )
 
     dispatch(action)
 
